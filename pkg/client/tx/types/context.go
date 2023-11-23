cae84c9680dc44ca3ab310ae3b7467a5ff5ba2dc
harry <53987565+h5law@users.noreply.github.com>
2023-11-23 20:16:57 +0000

    chore: address comments

--- pkg/client/tx/types/context.go
+++ pkg/client/tx/types/context.go
@@ -4,8 +4,9 @@ import (
 	cosmosclient "github.com/cosmos/cosmos-sdk/client"
 )
 
-// Context is used to distinguish a cosmosclient.Context intended for use
-// in transactions from others.
-// This type is intentionally not an alias in order to make this distinction clear
-// to the dependency injector
+// Context is used to distinguish a cosmosclient.Context intended for use in
+// transactions from others. This is because the same cosmosclient.Context can
+// be used for both queries and transactions (as they are generated identically)
+// This type is intentionally not an alias in order to make this distinction
+// clear to the dependency injector
 type Context cosmosclient.Context
